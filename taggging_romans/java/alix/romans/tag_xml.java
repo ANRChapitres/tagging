052211065a1a6c963f517d57e0d297ba2c7bd9e5
root <root@odysseus>
2018-01-25 11:59:37 +0100

    first_attempt (waiting for tests)

--- taggging_romans/java/alix/romans/tag_xml.java
+++ taggging_romans/java/alix/romans/tag_xml.java
@@ -6,13 +6,16 @@ import alix.util.Occ;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -20,101 +23,130 @@ import org.jsoup.select.Elements;
 import org.xml.sax.SAXException;
 
 
+@SuppressWarnings("deprecation")
 public class tag_xml {
-	
-	public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException, TransformerException{
+
+	public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException, TransformerException {
 		
-		File dir = new File("/home/odysseus/Bureau/ANR/code/global_array/test_files/");
-		File[] files_list = dir.listFiles(new FilenameFilter() {
-		    public boolean accept(File dir, String name) {
-		        return name.toLowerCase().endsWith(".xml");
-		    }
-		});
+		System.out.println("This software tags TEI/XML files and exports tagged texts to a target director");
 		
-		for (File file : files_list){
+		if (args.length == 0) {
+			System.out.println("Usage : tag_xml.java ./path/to/your/source_directory ./path/to/your/target_directory");
+		}
+
+//		File input_dir = new File(args[0]);
+		File input_dir = new File("/root/git/tagging/taggging_romans/source_texts");
+//		File output_dir = new File(args[1]);
+		File output_dir = new File("/root/git/tagging/taggging_romans/target_texts");
+		File[] files_list = input_dir.listFiles(new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return name.toLowerCase().endsWith(".xml");
+				
+			}
 			
+		});
+
+		for (File file : files_list){
+
 			String text=new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
 			Tokenizer tokenizer=new Tokenizer(text);
 			Occ occ = new Occ();
-			
+
 			int num_words=0;
 			while (tokenizer.token(occ)) {
+				
 				if (!occ.graph().toString().contains("§")&&!occ.graph().toString().contains("¶")){
+					
 					num_words++;
+					
 				}
+				
 			}
-			
+
 			String list_keys[]=new String [num_words];
 			String list_lems[]=new String [num_words];
 			String list_tags[]=new String [num_words];
 			int index=0;
-			
+
 			tokenizer=new Tokenizer(text);
 			while (tokenizer.token(occ)) {
+				
 				if (!occ.graph().toString().contains("§")&&!occ.graph().toString().contains("¶")){
+					
 					list_keys[index]=occ.graph().toString();
 					list_lems[index]=occ.lem().toString();
 					list_tags[index]=occ.tag().toString();
 					index++;
+					
 				}
+				
 			}
-			
+
 			Document doc = Jsoup.parse(text);
 			Elements ps = doc.select("p");
-			
 
-			System.out.println(ps.size());
-			
 			for (Element p:ps){
 				
+				String p_text=p.text();
+				
 				if (p.text().length()>0){
+					
 					Occ occurrence = new Occ();
 					Tokenizer tok=new Tokenizer(p.text());
+					
 					while (tok.token(occurrence)) {
-//						System.out.println(occurrence.graph());
-//						if (occurrence.graph().toString().length()>0){
-							StringBuilder p_text = new StringBuilder(p.text());
+						
+						Pattern pattern=Pattern.compile("\\Q"+occurrence.graph().toString()+"\\E",Pattern.CASE_INSENSITIVE);
+						Matcher matcher = pattern.matcher(p.text());
+						
+						if (matcher.find() || occurrence.graph().toString().contains("…")) {
+							
 							StringBuilder replacement = new StringBuilder();
 							replacement.append(occurrence.graph().toString());
-							replacement.append(" <word form=");
+							replacement.append(" <word form=\"");
 							replacement.append(occurrence.graph().toString());
+							replacement.append(" lemma=\"");
+							replacement.append(occurrence.lem().toString());
+							replacement.append(" postag=\"");
+							replacement.append(occurrence.tag().toString());
 							replacement.append("\"> ");
-//							System.out.println(occurrence.graph().toString());
-//							System.out.println(p_text);
-//							System.out.println("premier index : "+p_text.toString().indexOf(occurrence.graph().toString()));
-//							System.out.println("deuxième index : "+p_text.toString().indexOf(occurrence.graph().toString())+occurrence.graph().toString().length());
-							String final_p=p_text.toString().replaceFirst("^(?!=)\\Q"+occurrence.graph().toString()+"\\E", replacement.toString());
-//							if (final_p.indexOf(occurrence.graph().toString())>0){
-//								System.out.println("string repérée");
-//							}
-							p.text(final_p);
-//						}
+							Element child_word=new Element("word");
+							child_word.text(replacement.toString());
+							p.appendChild(child_word);
+							
+						}
+						
+						else {
+							
+							System.out.println("Attention, ce terme n'est pas taggé : "+occurrence.graph().toString());
+							
+						}
+						
 					}
-					System.out.println("paragraphe résultant : "+p.text());
+					
+					if (p.ownText().endsWith(".")) {
+						
+						Element punct=new Element("word");
+						punct.text(". <word form=\".\" lemma=\".\" postag=\"PUN\">");
+						p.appendChild(punct);
+						
+					}
+					
+					p_text=p.text().replaceAll("\\Q"+p.ownText()+"\\E", "");
+					p.text(p_text);
+					
 				}
+				
 			}
 			
-			
-			
-			
-//			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-//			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-//			Document doc = dBuilder.parse(file);
-//			doc.getDocumentElement().normalize();
-//			NodeList nodes=doc.getElementsByTagName("p");
-//			
-//			for (String key:list_keys){
-//				Pattern pattern = Pattern.compile("(!?<)"+key);
-//				
-//				for (int i = 0; i < nodes.getLength(); i++) {
-//					Matcher matcher = pattern.matcher(nodes.item(i).getTextContent());
-//					if (matcher.find()){
-//						String replacement=nodes.item(i).getTextContent().re;
-//						
-//					}
-//				}
-//			}
+			File output = new File(output_dir+"/"+file.getName());
+			doc.outputSettings().indentAmount(0).prettyPrint(false);
+			@SuppressWarnings("resource")
+			PrintWriter writer = new PrintWriter(output,"utf-8");
+			writer.write(StringEscapeUtils.unescapeXml(doc.html())) ;
+
 		}
+		
 	}
-	
+
 }
